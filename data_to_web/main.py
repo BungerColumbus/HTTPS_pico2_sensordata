610f6b78946fb1084edf51cb57713c96aaef016e
BungerColumbus <erceanu.vlad@gmail.com>
2026-05-26 16:36:04 +0200

    Adding the sensor data to the data sent to the website

--- data_to_web/main.py
+++ data_to_web/main.py
@@ -1,7 +1,8 @@
-from machine import Pin, time_pulse_us
+from machine import Pin, time_pulse_us, reset
 from ultrasound import get_distance
 from utime import sleep
 import time
+import network_connection
 
 pin = Pin("LED", Pin.OUT)
 trig = Pin(14, Pin.OUT)
@@ -11,16 +12,17 @@ sleep(1) # sleep 1sec
 pin.toggle()
 print("Pico ON")
 
+try:
+    network_connection.connect()
+except KeyboardInterrupt:
+    reset()
 
+counter = 0
 
 while True:
     distance = get_distance(echo = echo, trig = trig)
     print(distance)
-    time.sleep_ms(100)
-    print("waited 0.1 sec")
-
-
-
-      
-print("Finished.")
-pin.off()
+    counter += 1
+    status = f"Hello from Pico 2W!\nUpdate #{counter}\nDistance: {distance} cm"
+    network_connection.send_data(status)
+    time.sleep_ms(10000)
